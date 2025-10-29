f7e766c5c22e79f5d694c0ac79cb69595a36b639
lakshya <lakshya10171@gmail.com>
2025-10-29 15:49:57 +0530

    add user Repo

---
+++ src/main/java/com/lakshya/auction/live_auction_service/Repository/BidRepo.java
@@ -0,0 +1,9 @@
+package com.lakshya.auction.live_auction_service.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lakshya.auction.live_auction_service.Entity.Bid;
+
+public interface BidRepo extends JpaRepository<Long , Bid> {
+
+    
+}
