a7d55dc4795a1a0435c34bd9511debc72ebfa2c3
lakshya <lakshya10171@gmail.com>
2025-10-29 14:54:22 +0530

    Add Auction Item Entity

---
+++ src/main/java/com/lakshya/auction/live_auction_service/Entity/AuctionItem.java
@@ -0,0 +1,46 @@
+package com.lakshya.auction.live_auction_service.Entity;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Auction_item")
+public class AuctionItem {
+    // define the properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal startPrice;
+
+    @Column(nullable = false)
+    private BigDecimal currentBid;
+
+    private Long highBidderUserId;
+
+    @Column(nullable = false)
+    private Instant endTime;
+    
+    @Column(nullable = false)
+    private AuctionStatus status = AuctionStatus.PENDING;
+
+    // define the Auction Status;
+    public enum AuctionStatus{
+        PENDING,ACTIVE,CLOSED
+    }
+}
