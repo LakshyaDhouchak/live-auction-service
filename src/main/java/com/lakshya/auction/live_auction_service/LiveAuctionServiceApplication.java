6711e89a4074a733c6cb8859d0da63c1ed7c8739
lakshya <lakshya10171@gmail.com>
2025-10-29 13:49:17 +0530

    first commit

---
+++ src/main/java/com/lakshya/auction/live_auction_service/LiveAuctionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lakshya.auction.live_auction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveAuctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveAuctionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakshya/auction/live_auction_service/LiveAuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshya.auction.live_auction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveAuctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
