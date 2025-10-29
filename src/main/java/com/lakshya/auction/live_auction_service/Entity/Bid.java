043b5cb0ca0b92a185f31e519dbe96674b65f6d7
lakshya <lakshya10171@gmail.com>
2025-10-29 15:25:45 +0530

    add the bid Entity

---
+++ src/main/java/com/lakshya/auction/live_auction_service/Entity/Bid.java
@@ -0,0 +1,42 @@
+package com.lakshya.auction.live_auction_service.Entity;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "bids")
+public class Bid {
+    // define the properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_item_id",nullable = false)
+    private AuctionItem auctionItem;
+
+    @Column(nullable = false)
+    private Long bidderUserId;
+
+    @Column(nullable = false)
+    private  BigDecimal amount;
+
+    @Column(nullable = false)
+    private Instant bidTime = Instant.now();
+
+    private boolean isSuccessful = false;
+    
+
+}
