6858579c93fa9c1202d5f533114af4fcb79d03f6
lakshya <lakshya10171@gmail.com>
2025-10-29 15:50:09 +0530

    add auction Repo

---
+++ src/main/java/com/lakshya/auction/live_auction_service/Repository/AuctionRepo.java
@@ -0,0 +1,8 @@
+package com.lakshya.auction.live_auction_service.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lakshya.auction.live_auction_service.Entity.AuctionItem;
+
+public interface AuctionRepo extends JpaRepository<Long , AuctionItem> {
+    
+}
