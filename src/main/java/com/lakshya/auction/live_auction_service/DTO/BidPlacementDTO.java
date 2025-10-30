40efd7f56f523ee13f6477f90b00563d063582b5
lakshya <lakshya10171@gmail.com>
2025-10-30 11:19:43 +0530

    add bidPlcaement DTO

---
+++ src/main/java/com/lakshya/auction/live_auction_service/DTO/BidPlacementDTO.java
@@ -0,0 +1,18 @@
+package com.lakshya.auction.live_auction_service.DTO;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class BidPlacementDTO {
+    // define the properties
+    @NotNull(message = "Auction Item ID is required")
+    private Long auctionItemId;
+
+    @NotNull(message = "Bid anount is required")
+    @DecimalMin(value = "0.01",inclusive = true ,message = "Bid ammount must be greater than zero")
+    private BigDecimal amount;
+}
