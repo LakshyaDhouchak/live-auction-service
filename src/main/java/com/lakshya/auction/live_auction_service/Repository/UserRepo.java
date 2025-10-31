479f979a51c2e90afaadd545511d8279fb1776fe
lakshya <lakshya10171@gmail.com>
2025-10-31 13:38:34 +0530

    add userRepo

--- src/main/java/com/lakshya/auction/live_auction_service/Repository/UserRepo.java
+++ src/main/java/com/lakshya/auction/live_auction_service/Repository/UserRepo.java
@@ -1,8 +1,9 @@
 package com.lakshya.auction.live_auction_service.Repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakshya.auction.live_auction_service.Entity.User;
 
-public interface UserRepo extends JpaRepository<Long ,User > {
-    
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
