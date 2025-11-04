94cda5ca7b0ee9b89f45797a2b10a8cde892afa5
lakshya <lakshya10171@gmail.com>
2025-11-04 15:24:32 +0530

    update Security Config

--- src/main/java/com/lakshya/auction/live_auction_service/Config/WebSocketConfig.java
+++ src/main/java/com/lakshya/auction/live_auction_service/Config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry ){
         registry.addEndpoint("/live-bids")
-        .setAllowedOrigins("/*")
+        .setAllowedOrigins("*")
         .withSockJS();
     }
 
