1bc3fc68e1ff940afe02387f8fd5c4a8807e9f6c
lakshya <lakshya10171@gmail.com>
2025-11-06 06:05:20 +0530

    updated liveAuctionServiceApllication

--- src/main/java/com/lakshya/auction/live_auction_service/LiveAuctionServiceApplication.java
+++ src/main/java/com/lakshya/auction/live_auction_service/LiveAuctionServiceApplication.java
@@ -2,8 +2,13 @@ package com.lakshya.auction.live_auction_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.lakshya.auction.live_auction_service.Repository")
+@EntityScan(basePackages = "com.lakshya.auction.live_auction_service.Entity")
+
 public class LiveAuctionServiceApplication {
 
 	public static void main(String[] args) {
